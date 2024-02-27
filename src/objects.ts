02efec6a9c6b213c38c52ad5601c39fc957c77f2
Jas Saini <jas.saini@udel.edu>
2024-02-26 20:40:22 -0500

    Finished Task 6

--- src/objects.ts
+++ src/objects.ts
@@ -147,5 +147,12 @@ export function mergeQuestion(
     contentQuestion: Question,
     { points }: { points: number }
 ): Question {
-    return contentQuestion;
+    const merged = {
+        ...contentQuestion,
+        id: id,
+        name: name,
+        points: points,
+        published: false
+    };
+    return merged;
 }
