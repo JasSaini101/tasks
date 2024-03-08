546b7f6aa7dbd6b6cbab2e1697cf1ee66dca3a6d
Jasnoor Saini <jasnoor.saini@icloud.com>
2024-03-07 22:17:27 -0500

    Solved Change Color

--- src/form-components/ChangeColor.tsx
+++ src/form-components/ChangeColor.tsx
@@ -1,9 +1,91 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function ChangeColor(): JSX.Element {
+    const [color, setColor] = useState<string>("black");
+
+    function updateColor(event: React.ChangeEvent<HTMLInputElement>) {
+        setColor(event.target.value);
+    }
+
     return (
         <div>
             <h3>Change Color</h3>
+            <Form.Check
+                inline
+                type="radio"
+                id="red"
+                label="Red"
+                value="red"
+                checked={color === "red"}
+                onChange={updateColor}
+            />
+            <Form.Check
+                inline
+                type="radio"
+                id="green"
+                label="Green"
+                value="green"
+                checked={color === "green"}
+                onChange={updateColor}
+            />
+            <Form.Check
+                inline
+                type="radio"
+                id="blue"
+                label="Blue"
+                value="blue"
+                checked={color === "blue"}
+                onChange={updateColor}
+            />
+            <Form.Check
+                inline
+                type="radio"
+                id="orange"
+                label="Orange"
+                value="orange"
+                checked={color === "orange"}
+                onChange={updateColor}
+            />
+            <Form.Check
+                inline
+                type="radio"
+                id="pink"
+                label="Pink"
+                value="pink"
+                checked={color === "pink"}
+                onChange={updateColor}
+            />
+            <Form.Check
+                inline
+                type="radio"
+                id="purple"
+                label="Purple"
+                value="purple"
+                checked={color === "purple"}
+                onChange={updateColor}
+            />
+            <Form.Check
+                inline
+                type="radio"
+                id="white"
+                label="White"
+                value="white"
+                checked={color === "white"}
+                onChange={updateColor}
+            />
+            <Form.Check
+                inline
+                type="radio"
+                id="brown"
+                label="Brown"
+                value="brown"
+                checked={color === "brown"}
+                onChange={updateColor}
+            />
+            <div data-testid="colored-box" style={{ backgroundColor: color }}>
+                The chosen color is {color}
+            </div>
         </div>
     );
 }
--- src/form-components/EditMode.tsx
+++ src/form-components/EditMode.tsx
@@ -1,9 +1,40 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function EditMode(): JSX.Element {
+    const [editMode, setEditMode] = useState<boolean>(false);
+    const [isStudent, setisStudent] = useState<boolean>(true);
+    const [studentName, setStudentName] = useState<string>("Your Name");
+
+    function updateEditMode(event: React.ChangeEvent<HTMLInputElement>) {
+        setEditMode(!editMode);
+    }
+
+    function updateisStudent(event: React.ChangeEvent<HTMLInputElement>) {
+        setisStudent(!isStudent);
+    }
+
     return (
         <div>
-            <h3>Edit Mode</h3>
+            <Form.Switch
+                type="switch"
+                id="switch-mode"
+                label="Edit?"
+                checked={editMode}
+                onChange={updateEditMode}
+            />
+            <Form.Check
+                type="checkbox"
+                id="is-checkbox"
+                label="student?"
+                checked={isStudent}
+                onChange={updateisStudent}
+            />
+            <div>
+                <h3>
+                    {studentName} is {isStudent ? "a student" : "not a student"}
+                </h3>
+            </div>
         </div>
     );
 }
